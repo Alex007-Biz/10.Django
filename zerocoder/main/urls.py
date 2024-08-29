8c5c9030d8e8871690122805b40fe6dec3fdc49b
Alex007-Biz <bizukin@yandex.ru>
2024-08-29 22:03:38 +0300

    makemigrations

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('new', views.new,  name='page2'),
-    path('new2', views.new, name='page3')
+    path('new2', views.new2, name='page3')
 ]
