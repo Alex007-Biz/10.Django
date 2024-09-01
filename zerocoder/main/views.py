3a91057911e69555ba494e0ae85a8e1d21b8a5e8
Alex007-Biz <bizukin@yandex.ru>
2024-09-01 16:04:16 +0300

    9

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News_post
+from .forms import News_postForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -21,4 +22,14 @@ def new3(request):
     return HttpResponse("<h1>Это третья страница проекта на Django</h1>")
 
 def create_news(request):
-    return render(request, 'main/add_new_post.html')
+    error = ""
+    if request.method == 'POST':
+        form = News_postForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            errors = "Данные были заполнены некорректно"
+
+    form = News_postForm()
+    return render(request, 'main/add_new_post.html', {'form' : form, 'errors': error})
